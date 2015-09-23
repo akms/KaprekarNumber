bc613ef721d8c3c0072b41f2426ff61382143662
Akimasa Takeuchi <akimasa.takeuchi@gaiax.com>
2015-09-24 01:54:21 +0900

    add definition 2

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 	"unicode/utf8"
@@ -28,7 +29,7 @@ func make_str_slice(square_str string) []string {
 	return square_slice
 }
 
-func make_check_num(square_slice []string, digit int) (check_num int) {
+func make_check_num_one(square_slice []string, digit int) (check_num int) {
 	str_num_head := square_slice[0:(digit / 2)]
 	str_num_tail := square_slice[(digit / 2):digit]
 	int_num_head, _ := strconv.Atoi(strings.Join(str_num_head, ""))
@@ -37,28 +38,71 @@ func make_check_num(square_slice []string, digit int) (check_num int) {
 	return
 }
 
-func check_kaprekar_number(int_number int, check_num int) {
+func check_kaprekar_number_one(int_number int, check_num int) {
 	if int_number == check_num {
-		fmt.Printf("%d is Kaprekar Number\n", int_number)
+		fmt.Printf("%d is Kaprekar Number. (Definition 1)\n", int_number)
 	} else {
-		fmt.Printf("%d is not Kaprekar Number\n", int_number)
+		fmt.Printf("%d is not Kaprekar Number. (Definition 1)\n", int_number)
 	}
 }
 
+func kaprekar_number_one(int_number int) {
+	var square_str string
+	var digit, square_num, check_num int
+
+	square_num = square(int_number)
+	square_str = strconv.Itoa(square_num)
+	digit = utf8.RuneCountInString(square_str)
+	square_slice := make_str_slice(square_str)
+	check_num = make_check_num_one(square_slice, digit)
+	check_kaprekar_number_one(int_number, check_num)
+}
+
+func ascending_sort_str_slice(ascending_str_slice []string) []string {
+	sort.Sort(sort.StringSlice(ascending_str_slice))
+	return ascending_str_slice
+}
+
+func descending_sort_str_slice(descending_str_slice []string) []string {
+	sort.Sort(sort.Reverse(sort.StringSlice(descending_str_slice)))
+	return descending_str_slice
+}
+
+func make_check_num_tw(str_slice []string) (check_num int) {
+	check_num, _ = strconv.Atoi(strings.Join(str_slice, ""))
+	return
+}
+
+func check_kaprekar_number_tw(int_number, descending_num, ascending_num int) {
+	var ans_num int
+	ans_num = descending_num - ascending_num
+	if ans_num == int_number {
+		fmt.Printf("%d is Kaprekar Number. (Definition 2)\n", int_number)
+	} else {
+		fmt.Printf("%d is not Kaprekar Number. (Definition 2)\n", int_number)
+	}
+}
+
+func kaprekar_number_tw(str_number string, int_number int) {
+	str_slice := make_str_slice(str_number)
+	ascending_order := ascending_sort_str_slice(str_slice)
+	ascending_num := make_check_num_tw(ascending_order)
+	descending_order := descending_sort_str_slice(str_slice)
+	descending_num := make_check_num_tw(descending_order)
+
+	check_kaprekar_number_tw(int_number, descending_num, ascending_num)
+}
+
 func main() {
-	var str_number, square_str string
-	var digit, int_number, square_num, check_num int
+	var str_number string
+	var int_number int
 	fmt.Print("Please enter number \n> ")
 
 	str_number = scan()
 	int_number, _ = strconv.Atoi(str_number)
-	square_num = square(int_number)
-	fmt.Println(square_num)
-	square_str = strconv.Itoa(square_num)
-	digit = utf8.RuneCountInString(square_str)
-	square_slice := make_str_slice(square_str)
-	check_num = make_check_num(square_slice, digit)
-	check_kaprekar_number(int_number, check_num)
+
+	kaprekar_number_one(int_number)
+	kaprekar_number_tw(str_number, int_number)
 
 	return
 }
